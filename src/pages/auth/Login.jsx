339950a257fa77cd02aec05db4434aae935412e4
EunSo0 <dmstn423@naver.com>
2024-01-02 17:12:24 +0900

    [FEAT] 로그인, 회원가입

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,37 +1,61 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import { styled } from "styled-components";
 
 export default function Login() {
-  const [state, setState] = useState({});
+  const [info, setInfo] = useState({});
   const { register, handleSubmit } = useForm();
+  const navigate = useNavigate();
 
   const onSubmit = (data) => {
-    setState(data);
-    console.log(data); // 수정된 부분: 직접 받아온 데이터 출력
+    setInfo(data);
+    console.log(data);
+  };
+
+  const goToSign = () => {
+    navigate("/signin");
   };
 
   return (
-    <Wrapper onSubmit={handleSubmit(onSubmit)}>
-      <InputBox
-        name="email"
-        placeholder="이메일을 입력해주세요"
-        {...register("email")}
-      />
-      <InputBox
-        name="password"
-        placeholder="비밀번호를 입력해주세요"
-        type="password"
-        {...register("password")}
-      />
+    <Wrapper>
+      <Title>
+        반가워요!
+        <br />
+        우리 같이 시작해볼까요?
+      </Title>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <InputBox
+          name="email"
+          placeholder="이메일을 입력해주세요"
+          {...register("email")}
+        />
+        <InputBox
+          name="password"
+          placeholder="비밀번호를 입력해주세요"
+          type="password"
+          {...register("password")}
+        />
 
-      <LoginBtn type="submit">로그인</LoginBtn>
-      <SigninBtn>회원가입</SigninBtn>
+        <LoginBtn type="submit">로그인</LoginBtn>
+        <SigninBtn onClick={goToSign}>회원가입</SigninBtn>
+      </Form>
     </Wrapper>
   );
 }
 
-const Wrapper = styled.form`
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+const Title = styled.div`
+  font-size: 24px;
+  font-weight: 700;
+  margin-bottom: 80px;
+`;
+
+const Form = styled.form`
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -45,6 +69,7 @@ const InputBox = styled.input`
   border: 1px solid #000;
   padding: 12px;
 `;
+
 const LoginBtn = styled.button`
   width: 308px;
   height: 54px;
--- src/pages/auth/Signin.jsx
+++ src/pages/auth/Signin.jsx
@@ -1,24 +1,55 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import { styled } from "styled-components";
+import styled, { css } from "styled-components";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const validationSchema = yup.object().shape({
+  email: yup
+    .string()
+    .required("이메일을 입력해주세요")
+    .email("이메일 형식이 아닙니다."),
+  password: yup
+    .string()
+    .required("영문, 숫자포함 8자리를 입력해주세요.")
+    .min(8, "최소 8자 이상 가능합니다")
+    .max(15, "최대 15자 까지만 가능합니다")
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,15}$/,
+      "영문 숫자포함 8자리를 입력해주세요."
+    ),
+  passwordConfirm: yup
+    .string()
+    .required("비밀번호 확인을 해주세요.")
+    .oneOf([yup.ref("password")], "비밀번호가 다릅니다."),
+});
 
 export default function Login() {
-  const [state, setState] = useState({});
-  const { register, handleSubmit } = useForm();
+  const [info, setInfo] = useState({});
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: "onChange",
+    reValidateMode: "onChange",
+    resolver: yupResolver(validationSchema), // Use yup resolver for validation
+  });
 
   const onSubmit = (data) => {
-    setState(data);
-    console.log(data); // 수정된 부분: 직접 받아온 데이터 출력
+    setInfo(data);
+    console.log(data);
   };
 
   return (
     <Wrapper onSubmit={handleSubmit(onSubmit)}>
-      <Label>아이디</Label>
+      <Label first>아이디</Label>
       <InputBox
         name="email"
         placeholder="이메일을 입력해주세요"
         {...register("email")}
       />
+      {errors.email && <ErrorText>{errors.email.message}</ErrorText>}
 
       <Label>비밀번호</Label>
       <InputBox
@@ -27,14 +58,18 @@ export default function Login() {
         type="password"
         {...register("password")}
       />
+      {errors.password && <ErrorText>{errors.password.message}</ErrorText>}
 
       <Label>비밀번호 재확인</Label>
       <InputBox
-        name="repassword"
+        name="passwordConfirm"
         placeholder="확인을 위해 비밀번호를 입력해주세요"
         type="password"
-        {...register("repassword")}
+        {...register("passwordConfirm")}
       />
+      {errors.passwordConfirm && (
+        <ErrorText>{errors.passwordConfirm.message}</ErrorText>
+      )}
 
       <SubmitBtn type="submit">다음 단계로 이동</SubmitBtn>
     </Wrapper>
@@ -45,10 +80,42 @@ const Wrapper = styled.form`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
 `;
-const Label = styled.div``;
+const Label = styled.div`
+  font-size: 24px;
+  font-weight: 700;
+  margin-top: 42px;
+  margin-bottom: 32px;
+
+  ${(props) =>
+    props.first &&
+    css`
+      margin-top: 0;
+    `}
+`;
 const InputBox = styled.input`
-  margin-bottom: 10px;
+  width: 308px;
+  height: 54px;
+  border-radius: 10px;
+  border: 1px solid #000;
+  font-size: 15px;
+  font-weight: 400;
+  margin-bottom: 8px;
+  padding: 12px;
 `;
-const SubmitBtn = styled.button``;
+const ErrorText = styled.div`
+  font-size: 12px;
+  color: red;
+  margin: 0 0 12px 6px;
+`;
+const SubmitBtn = styled.button`
+  width: 308px;
+  height: 54px;
+  border: none;
+  border-radius: 10px;
+  background: #848484;
+  color: #fff;
+  text-align: center;
+  font-size: 20px;
+  margin-top: 32px;
+`;
